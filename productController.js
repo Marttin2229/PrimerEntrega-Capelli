1040f12303a567433afbd4fb4ba8940ee36f08e4
Marttin2229 <62270209+Marttin2229@users.noreply.github.com>
2024-01-24 14:53:15 -0300

    Add files via upload

---
+++ productController.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const path = require('path');
+const productsFilePath = path.join(__dirname, 'data', 'products.json');
+
+const getAllProducts = (req, res) => {
+};
+
+const getProductById = (req, res) => {
+};
+
+const addProduct = (req, res) => {
+};
+
+const updateProduct = (req, res) => {
+};
+
+const deleteProduct = (req, res) => {
+};
+
+module.exports = {
+  getAllProducts,
+  getProductById,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
